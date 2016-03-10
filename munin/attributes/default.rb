1cf1b224a8e9ab5eff400b5927041bb08ca830f9
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2016-03-10 22:09:54 +0000

    adding a cookbook for munin-node only

---
+++ munin/attributes/default.rb
@@ -0,0 +1,5 @@
+default["munin"]["host_name"] = "SET MUNIN HOST NAME"
+default["munin"]["port"] = "4949"
+default["munin"]["plugins"] = []
+default["munin"]["allow"] = ["^127."]
+default["munin"]["cidr_allow"] = []
---
+++ munin/recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: munin
+# Recipe:: default
+#
+# Copyright 2016, Corporate Rewards
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# get the package installed
+package 'munin-node' do
+  action :install
+end
+
+# add our config file from a template
+template "/etc/munin/munin-node.conf" do
+  mode '0644'
+  owner "root"
+  group "root"
+  source "munin-node.conf.erb"
+end
+
+# add any extra plugins - if they and their congig exist
+node["munin"]["plugins"].each |plugin| do
+  cookbook_file "/usr/local/munin/plugins/#{plugin}" do
+    only_if { run_context.has_cookbook_file_in_cookbook?(cookbook_name, "plugin/#{plugin}") }
+    mode 0644
+    source "plugins/#{plugin}"
+  end
+  cookbook_file "/usr/local/munin/plugins/zzz_opsworks_#{plugin_conf}.conf" do
+    only_if { run_context.has_cookbook_file_in_cookbook?(cookbook_name, "plugin/#{plugin_conf}.conf") }
+    mode 0644
+    source "plugins/#{plugin}.conf"
+  end
+end
+
+# run the oneliner to configure munin
+execute "munin-node-configure" do
+  command "munin-node-configure --shell --families=contrib,auto | sh -x"
+end
+
+# start 'er up
+service 'munin-node' do
+  action [ :enable, :start ]
+end
+
+
