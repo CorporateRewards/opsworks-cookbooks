eb967de8d73e40f386f5a4b2733e65e4a1d7501f
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2016-03-11 09:19:11 +0000

    adding a defaultnode of localhost for munin-master attributes

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -3,3 +3,4 @@ default["munin-node"]["port"] = "4949"
 default["munin-node"]["plugins"] = []
 default["munin-node"]["allow"] = ["^127."]
 default["munin-node"]["cidr_allow"] = []
+default["munin-master"]["nodes"] = [{name:"localhost",address:"127.0.0.1",use_node_name:"yes"}]
