459130561be46519e50bd61e87fb713af89bb8cb
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-03-08 14:13:01 +0000

    fix access to deploy variables

--- codeigniter/recipes/configure.rb
+++ codeigniter/recipes/configure.rb
@@ -3,14 +3,14 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  directory "#{node[:default][:application][:deploy_to]}/shared/tmp/cache" do
+  directory "#{deploy[:deploy_to]}/shared/tmp/cache" do
     mode 0755
     group deploy[:group]
     owner deploy[:user]
     action :create
   end
 
-  directory "#{node[:default][:application][:deploy_to]}/shared/tmp/sessions" do
+  directory "#{deploy[:deploy_to]}/shared/tmp/sessions" do
     mode 0755
     group deploy[:group]
     owner deploy[:user]
