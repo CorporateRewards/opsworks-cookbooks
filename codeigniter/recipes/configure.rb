58761c0c90b40019ed32d25137e58f0804589460
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-03-08 14:36:15 +0000

    permissions on paths

--- codeigniter/recipes/configure.rb
+++ codeigniter/recipes/configure.rb
@@ -4,14 +4,14 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   directory "#{deploy[:deploy_to]}/shared/tmp/cache" do
-    mode 0664
+    mode 0660
     group deploy[:group]
     owner deploy[:user]
     action :create
   end
 
   directory "#{deploy[:deploy_to]}/shared/tmp/sessions" do
-    mode 0664
+    mode 0660
     group deploy[:group]
     owner deploy[:user]
     action :create
