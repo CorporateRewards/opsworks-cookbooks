5f2207ceb8d081f23b4e9cd7bc3256a466c74f5b
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-03-08 10:54:05 +0000

    adding in a codeigniter recipe

---
+++ codeigniter/configure.rb
@@ -0,0 +1,15 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  template "#{deploy[:deploy_to]}/application/config/database.php" do
+    source "db.erb"
+    cookbook 'codeigniter'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:database => deploy[:database])
+
+  end
+end
