010f191a4b5cb3e4fcf6a0d4cb3da3dff46eba2d
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-03-13 12:00:25 +0000

    config session driver

--- codeigniter/attributes/default.rb
+++ codeigniter/attributes/default.rb
@@ -1,4 +1,5 @@
 default['codeigniter']['config']['log_threshold'] = 0
 default['codeigniter']['config']['log_path'] = 'log/'
 default['codeigniter']['config']['cache_path'] = 'application/cache/'
-default['codeigniter']['config']['session_save_path'] = 'tmp/sessions/'
+default['codeigniter']['config']['sess_save_path'] = 'tmp/sessions/'
+default['codeigniter']['config']['sess_driver'] = 'files'
