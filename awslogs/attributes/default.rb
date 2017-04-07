1c5691bcdc4794c36990bd18f05a7cdf62fe7127
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-04-07 11:35:35 +0100

    adding awslogs cookbook

---
+++ awslogs/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log' 
+default[:cwlogs][:region] = 'eu-west-1'
---
+++ awslogs/recipes/config.rb
@@ -0,0 +1,7 @@
+template "/tmp/cwlogs.cfg" do
+  cookbook "awslogs"
+  source "cwlogs.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
