38dbae925946e0ddd862ff4cf5f3a40275343118
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-11-16 12:29:07 +0000

    update recipe and config

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -5,4 +5,4 @@ default['sidekiq']['pid_dir'] = './log'
 default['sidekiq']['rails_env'] = 'production'
 
 default['sidekiq']['queues'] = {}
-default['sidekiq']['queues']['default'] = 'config/sidekiq.yml'
+default['sidekiq']['queues']['default'] = '/config/sidekiq.yml'
