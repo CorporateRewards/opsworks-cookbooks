8e84ef84574454eac4b0c44aa1a755e0309292fe
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-11-17 09:42:38 +0000

    tweaked recipe and added some debug output

--- opsworks_sidekiq/recipes/default.rb
+++ opsworks_sidekiq/recipes/default.rb
@@ -1,3 +1,8 @@
+# @Date:   2017-11-15T10:53:04+00:00
+# @Last modified time: 2017-11-17T09:42:08+00:00
+
+
+
 #
 # Cookbook Name:: opsworks_sidekiq
 # Recipe:: default
@@ -7,9 +12,10 @@
 node[:deploy].each do |application, deploy|
 
   node['sidekiq']['queues'].each do |name, config|
+    puts "Got config for Sidekiq Queue - #{name} => #{config}"
     opsworks_sidekiq "#{name}" do
-      config = config
-      queue_name = name
+      config config
+      queue_name name
       working_directory "/srv/www/#{application}/current"
       rails_env node[:rails_env]
       user "deploy"
