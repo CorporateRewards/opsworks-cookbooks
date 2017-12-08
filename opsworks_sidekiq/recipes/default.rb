adbbfda15d18ed9c0bf61daf684122f7299bf46f
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2017-12-08 16:23:48 +0000

    adding env vars from opsworks to start template

--- opsworks_sidekiq/recipes/default.rb
+++ opsworks_sidekiq/recipes/default.rb
@@ -3,8 +3,13 @@
 # Recipe:: default
 #
 #
+#
 
 node[:deploy].each do |application, deploy|
+  vars={}
+  deploy[:environment_variables].each do |key, value|
+    vars[key] = value
+  end
 
   node['sidekiq']['queues'].each do |name, config|
     puts "Got config for Sidekiq Queue - #{name} => #{config}"
@@ -15,6 +20,7 @@ node[:deploy].each do |application, deploy|
       rails_env node[:rails_env]
       user "deploy"
       group "www-data"
+      vars vars
     end
   end
 end
