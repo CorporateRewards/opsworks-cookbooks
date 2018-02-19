f097cdc11af2d9ace05b7d6c0688db7d6b9b577c
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-19 17:02:06 +0000

    name spacing directories by app

--- codeigniter/recipes/configure.rb
+++ codeigniter/recipes/configure.rb
@@ -5,25 +5,25 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   directory "#{deploy[:deploy_to]}/shared/cache" do
-    mode 0660
+    mode 0755
     group node[:apache][:user]
     owner node[:apache][:user]
   end
 
   directory "#{deploy[:deploy_to]}/shared/sessions" do
-    mode 0660
+    mode 0755
     group node[:apache][:user]
     owner node[:apache][:user]
   end
 
   directory "#{deploy[:deploy_to]}/shared/cache/#{application}" do
-    mode 0660
+    mode 0755
     group node[:apache][:user]
     owner node[:apache][:user]
   end
 
   directory "#{deploy[:deploy_to]}/shared/sessions/#{application}" do
-    mode 0660
+    mode 0755
     group node[:apache][:user]
     owner node[:apache][:user]
   end
