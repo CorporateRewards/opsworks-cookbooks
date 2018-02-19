d74e424179d045ae7bbaafa92652fe4aa0ba913a
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-19 16:48:48 +0000

    name spacing directories by app

--- codeigniter/recipes/configure.rb
+++ codeigniter/recipes/configure.rb
@@ -3,19 +3,17 @@ include_recipe "apache2"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  deploy[:apache][:user]
-
   directory "#{deploy[:deploy_to]}/shared/cache/#{application}" do
     mode 0660
-    group deploy[:apache][:user]
-    owner deploy[:apache][:user]
+    group node[:apache][:user]
+    owner node[:apache][:user]
     recursive true
   end
 
   directory "#{deploy[:deploy_to]}/shared/sessions/#{application}" do
     mode 0660
-    group deploy[:apache][:user]
-    owner deploy[:apache][:user]
+    group node[:apache][:user]
+    owner node[:apache][:user]
     recursive true
   end
 
