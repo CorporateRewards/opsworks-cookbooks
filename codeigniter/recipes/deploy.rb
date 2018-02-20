2362ae33aa19880f192fba47e6efc9c8ad5365dd
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-20 17:09:38 +0000

    This should deploy the correct templates for the app we are deploying

--- codeigniter/recipes/deploy.rb
+++ codeigniter/recipes/deploy.rb
@@ -3,25 +3,24 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
-
-  Chef::Log.info("Chef node :deploy data:")
-  Chef::Log.info(deploy)
-
-  template "#{deploy[:deploy_to]}/shared/config/#{application}/database.php" do
-    source "db.erb"
-    cookbook 'codeigniter'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(:database => deploy[:database])
-  end
-
-  template "#{deploy[:deploy_to]}/shared/config/#{application}/config.php" do
-    source "config.erb"
-    cookbook 'codeigniter'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(:config => node[:codeigniter][:config], :app => application)
+  if(deploy['deploy_to'] && File.directory?(deploy['deploy_to']))
+    template "#{deploy[:deploy_to]}/shared/config/#{application}/database.php" do
+      source "db.erb"
+      cookbook 'codeigniter'
+      mode "0660"
+      group deploy[:group]
+      owner deploy[:user]
+      variables(:database => deploy[:database])
+    end
+    Chef::Log.info("Created database template for #{application}")
+    template "#{deploy[:deploy_to]}/shared/config/#{application}/config.php" do
+      source "config.erb"
+      cookbook 'codeigniter'
+      mode "0660"
+      group deploy[:group]
+      owner deploy[:user]
+      variables(:config => node[:codeigniter][:config], :app => application)
+    end
+    Chef::Log.info("Created config template for #{application}")
   end
 end
