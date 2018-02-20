aac8ab026e457e005d5d2297ae1241fd9211d7c8
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-20 09:48:38 +0000

    Namespacing logs, cache and sessions in a better way

--- codeigniter/recipes/configure.rb
+++ codeigniter/recipes/configure.rb
@@ -4,16 +4,21 @@ include_recipe "apache2"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  directory "#{node[:codeigniter][:sess_save_path]}/#{application}" do
-    mode 0755
-    group node[:apache][:user]
-    owner node[:apache][:user]
-  end
+  sess_path = "#{node[:codeigniter][:sess_save_path]}/#{application}"
+  cache_path = "#{node[:codeigniter][:cache_path]}/#{application}"
 
-  directory "#{node[:codeigniter][:cache_path]}/#{application}" do
+  directory sess_path do
     mode 0755
     group node[:apache][:user]
     owner node[:apache][:user]
   end
+  Chef::log.info("Created Sessions directory: #{sess_path}")
+
+  directory cache_path do
+    mode 0755
+    group node[:apache][:user]
+    owner node[:apache][:user]
+  end
+  Chef::log.info("Created Cache directory: #{cache_path}")
 
 end
