7d9dcc95006ccd7c0baf318243674d4e6df9d8e5
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-21 10:34:11 +0000

    changed default value to blank for log path

--- codeigniter/attributes/default.rb
+++ codeigniter/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:codeigniter][:config][:log_threshold] = 4
-default[:codeigniter][:config][:log_path] = 'application/logs'
+default[:codeigniter][:config][:log_path] = ''
 default[:codeigniter][:config][:cache_path] = '/tmp/cache'
 default[:codeigniter][:config][:sess_save_path] = '/tmp/sessions'
 default[:codeigniter][:config][:sess_driver] = 'opsworks_files'
