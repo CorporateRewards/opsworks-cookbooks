5378c2988c24c8f474117e9aa4bdeee2dbcf545d
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-24 01:58:10 +0000

    tweaked logic to allow any paths with wildcards and better merging

--- awslogs/recipes/config.rb
+++ awslogs/recipes/config.rb
@@ -1,18 +1,26 @@
-
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   if node[:deploy].length == 1
     application = ""
   end
+
+  streams = node[:cwlogs][:streams]
+
   if deploy[:cwlogs] && deploy[:cwlogs][:streams]
-    node[:cwlogs][:streams].merge(deploy[:cwlogs][:streams])
+    streams = streams.merge(deploy[:cwlogs][:streams]) {|key, fromnode, fromdeploy| fromnode.merge fromdeploy}
   end
+
+  #Chef::Log.info("Debug streams:")
+  #Chef::Log.info(deploy[:cwlogs][:streams])
+  #Chef::Log.info(streams)
+
+
   template "/tmp/cwlogs.cfg" do
     cookbook "awslogs"
     source "cwlogs.cfg.erb"
     owner "root"
     group "root"
     mode 0644
-    variables(:streams => node[:cwlogs][:streams], :application => application, :deploy_to => deploy[:deploy_to])
+    variables(:streams => streams, :application => application, :deploy_to => deploy[:deploy_to])
   end
 end
