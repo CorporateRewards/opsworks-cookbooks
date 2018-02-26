d08f0d6cbc7ccb2cfa5366726b304b71f7fc6919
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-02-26 10:34:38 +0000

    copied codeigniter

---
+++ codeigniter/recipes/configure.rb
@@ -0,0 +1,26 @@
+include_recipe "apache2"
+
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  sess_path = "#{node[:codeigniter][:config][:sess_save_path]}/#{application}"
+  cache_path = "#{node[:codeigniter][:config][:cache_path]}/#{application}"
+
+  directory sess_path do
+    mode 0755
+    group node[:apache][:user]
+    owner node[:apache][:user]
+    recursive true
+  end
+  Chef::Log.info("Created Sessions directory: #{sess_path}")
+
+  directory cache_path do
+    mode 0755
+    group node[:apache][:user]
+    owner node[:apache][:user]
+    recursive true
+  end
+  Chef::Log.info("Created Cache directory: #{cache_path}")
+
+end
