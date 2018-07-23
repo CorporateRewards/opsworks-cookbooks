2155df0c9319a8b800cbdbc35b755e13d9c8a537
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-07-23 10:28:42 +0100

    tweaking repo setup

--- yarn/recipes/default.rb
+++ yarn/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'apt'
 
 apt_repository 'yarn' do
-  uri          'https://dl.yarnpkg.com/debian'
-  components   ['main', 'stable']
+  uri          'https://dl.yarnpkg.com/debian/'
+  components   ['stable', 'main']
   key          'https://dl.yarnpkg.com/debian/pubkey.gpg'
 end
