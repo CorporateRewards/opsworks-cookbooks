496f944c057209dcea69ff87cb23c82d11887b04
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-07-23 14:03:09 +0100

    can't subscribe to someehting outside my recipe/cookbook

--- yarn/recipes/install_modules.rb
+++ yarn/recipes/install_modules.rb
@@ -1,8 +1,6 @@
-include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   execute "yarn install" do
     command "yarn install"
-    subscribes :create, resources(:template => "#{node[:deploy][application][:deploy_to]}/shared/config/database.yml")
   end
 end
