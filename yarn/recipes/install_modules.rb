7a5abc66507b2e5089c35dd47b2125cf202ebb4c
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-07-23 16:13:16 +0100

    updated to use latest release to run yarn from

--- yarn/recipes/install_modules.rb
+++ yarn/recipes/install_modules.rb
@@ -2,6 +2,6 @@
 node[:deploy].each do |application, deploy|
   execute "yarn install" do
     command "cd $(ls -tdogl releases/* | awk '{print $7}' | head -n1) && yarn install"
+    cwd deploy["deploy_to"]
   end
-  cwd deploy["deploy_to"]
 end
