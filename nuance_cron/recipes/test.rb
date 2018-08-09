51c9785d9e1b6d86071ca30274356f0f00358afb
tedmoyses <Ted.Moyses@corporaterewards.co.uk>
2018-08-09 17:41:34 +0100

    user deploy

--- nuance_cron/recipes/test.rb
+++ nuance_cron/recipes/test.rb
@@ -1,4 +1,5 @@
 cron_d "cron_test" do
   minute "2"
   command "echo 'Testing' > /tmp/test.txt"
+  user "deploy"
 end
--- nuance_cron/recipes/user_import.rb
+++ nuance_cron/recipes/user_import.rb
@@ -2,4 +2,5 @@ cron_d "nuance_user_import" do
   hour "7"
   minute "5"
   command "cd /srv/www/nuancev3/current && php index.php tools/trigger_user_update"
+  user "deploy"
 end
